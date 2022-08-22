ca3c8167d3f19295618ac3ab1d3a421379a50606
Varun Chaubey <varunchaubey4@gmail.com>
2022-08-22 20:46:05 +0530

    add users routes and views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req, res) {
-    res.end('<h1>Hello World!</h1>')
-}
+    res.render('home', {
+        title: 'Home'
+    });
+};
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 8000;
 
 //use express router
 app.use('/', require('./routes'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err) {
