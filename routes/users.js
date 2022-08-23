ac5d6b1301973a0b8b1de3662e9480f0f3ebd5ad
Varun Chaubey <varunchaubey4@gmail.com>
2022-08-24 00:36:13 +0530

    add controllers for sign-in and sign-up

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,7 @@ const users_controller = require('../controllers/users_controller');
 router.get('/profile', users_controller.profile);
 router.get('/sign-up', users_controller.signUp);
 router.get('/sign-in', users_controller.signIn);
+router.post('/create', users_controller.create);
+router.post('/create-session', users_controller.createSession);
 
 module.exports = router;
