3bd6851eac6eb89395a42f39ad67deb6ee1b5c47
Varun Chaubey <varunchaubey4@gmail.com>
2022-12-27 15:44:56 +0530

    Add chat functionality

--- configs/chat_sockets.js
+++ configs/chat_sockets.js
@@ -3,5 +3,21 @@ module.exports.chatSockets = function(socketServer) {
 
     io.sockets.on('connection', function(socket) {
         console.log('New connection received', socket.id);
+        
+        socket.on('disconnect', function() {
+            console.log('Socket disconnected');
+        });
+
+        socket.on('join_room', function(data) {
+            console.log('Joining request received', data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined', data);
+        });
+
+        socket.on('send_message', function(data){
+            io.in(data.chatroom).emit('receive_message', data);
+        });
     });
 }
